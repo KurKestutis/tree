2ca895b6fa8eda2680a03965fb01fede543c6a91
Kestutis Kurienius <kurkestutis@gmail.com>
2021-12-30 16:45:33 +0200

    Added variable scanning from file and print tree in browser

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,41 @@
 const printTree = function (rowNumbers) {
+  let wholeTree = "";
   if (typeof rowNumbers == "undefined") {
     rowNumbers = 25;
   }
+
   let spaceInRow = "";
   let asterisksInRow = "";
   for (let currentRow = 1; currentRow <= rowNumbers; currentRow++) {
     for (let index = rowNumbers - currentRow; index > 0; index--) {
       spaceInRow += " ";
+      wholeTree += " ";
     }
 
     for (let index2 = currentRow; index2 > 0; index2--) {
       asterisksInRow += " *";
+      wholeTree += " *";
     }
 
     console.log(`${spaceInRow} ${asterisksInRow}`);
+    wholeTree += "\n";
     spaceInRow = "";
     asterisksInRow = "";
   }
+  return wholeTree;
 };
 
 printTree(8);
 
 printTree();
+
+document.getElementById("inputfile").addEventListener("change", function () {
+  let fileReader = new FileReader();
+  fileReader.onload = function () {
+    document.getElementById("output").textContent = printTree(
+      fileReader.result
+    );
+  };
+
+  fileReader.readAsText(this.files[0]);
+});
