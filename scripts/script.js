3f8b8f68f236215286513a8fb766e31c9d06f8a3
Kestutis Kurienius <kurkestutis@gmail.com>
2021-12-30 14:41:25 +0200

    printTree function created

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-alert("hi");
+const printTree = function (rowNumbers) {
+  let spaceInRow = "";
+  let asterisksInRow = "";
+  for (let currentRow = 1; currentRow <= rowNumbers; currentRow++) {
+    for (let index = rowNumbers - currentRow; index > 0; index--) {
+      spaceInRow += " ";
+    }
+
+    for (let index2 = currentRow; index2 > 0; index2--) {
+      asterisksInRow += " *";
+    }
+
+    console.log(`${spaceInRow} ${asterisksInRow}`);
+    spaceInRow = "";
+    asterisksInRow = "";
+  }
+};
+
+printTree(8);
